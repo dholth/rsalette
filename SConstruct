216de394e0f59bb9106ae4517b5a232a13922e75
Daniel Holth <dholth@fastmail.fm>
2016-08-27 19:39:58 -0400

    add enscons build

---
+++ SConstruct
@@ -0,0 +1,30 @@
+# Starter SConstruct for enscons
+
+import sys
+from distutils import sysconfig
+import pytoml as toml
+import enscons
+
+metadata = dict(toml.load(open('pyproject.toml')))['tool']['enscons']
+
+full_tag = 'py2.py3-none-any' # pure Python packages compatible with 2+3
+
+env = Environment(tools=['default', 'packaging', enscons.generate],
+                  PACKAGE_METADATA=metadata,
+                  WHEEL_TAG=full_tag,
+                  ROOT_IS_PURELIB=full_tag.endswith('-any'))
+
+# Only *.py is included automatically by setup2toml.
+# Add extra 'purelib' files or package_data here.
+py_source = ['rsalette.py'] + ['asn1lette.py']
+
+purelib = env.Whl('purelib', py_source, root='')
+whl = env.WhlFile(purelib)
+
+# It's easier to just use Glob() instead of FindSourceFiles() since we have
+# so few installed files..
+sdist_source=File(['PKG-INFO', 'SConstruct', 'pyproject.toml']) \
+        + Glob('*.py') + Glob('*.txt') + Glob('*.pem')
+sdist = env.SDist(source=sdist_source)
+env.NoClean(sdist)
+env.Alias('sdist', sdist)
--- setup.py
+++ setup.py
@@ -1,30 +1,18 @@
-import os
+#!/usr/bin/env python
+# Call enscons to emulate setup.py, installing if necessary.
 
-from setuptools import setup
+import sys, subprocess, os.path
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+sys.path[0:0] = ['setup-requires']
 
-tests_require = ["nose", "coverage"]
+try:
+    import enscons.setup
+except ImportError:
+    requires = ["enscons"]
+    subprocess.check_call([sys.executable, "-m", "pip", "install", 
+        "-t", "setup-requires"] + requires)
+    del sys.path_importer_cache['setup-requires'] # needed if setup-requires was absent
+    import enscons.setup
 
-setup(name='rsalette',
-      version='0.2.0',
-      description='A pure-Python 2+3 RSA verification library.',
-      long_description=README + '\n\n' +  CHANGES,
-      classifiers=[
-            "Topic :: Security :: Cryptography",
-            "License :: OSI Approved :: Apache Software License",
-        ],
-      author='Daniel Holth',
-      author_email='dholth@fastmail.fm',
-      url='http://bitbucket.org/dholth/rsalette/',
-      keywords='rsa',
-      license='ASL 2',
-      py_modules=['rsalette', 'asn1lette'],
-      include_package_data=True,
-      zip_safe=False,
-      tests_require = tests_require,
-      extras_require = dict(test=tests_require),
-      test_suite = 'nose.collector',
-      )
+enscons.setup.setup()
+
