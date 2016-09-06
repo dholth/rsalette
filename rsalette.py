55dd757a7c43371df24e8c7f88f1deec59a955f8
Daniel Holth <dholth@fastmail.fm>
2016-09-05 21:37:59 -0400

    basic jwt support, jwk alg support

--- rsalette.py
+++ rsalette.py
@@ -22,6 +22,7 @@ Simple pure-Python signature-verification-only RSA implementation.
 import base64
 import binascii
 import hashlib
+import json
 import re
 
 __all__ = ['PublicKey']
@@ -29,19 +30,25 @@ __all__ = ['PublicKey']
 ASN1_HASH = {b'0Q0\r\x06\t`\x86H\x01e\x03\x04\x02\x03\x05\x00\x04@': 'sha512', 
              b'0A0\r\x06\t`\x86H\x01e\x03\x04\x02\x02\x05\x00\x040': 'sha384', 
              b'010\r\x06\t`\x86H\x01e\x03\x04\x02\x01\x05\x00\x04 ': 'sha256'}
-             
+
+# json web algorithms to Python hash functions
+ALG_HASH = {'RS256': 'sha256', 'RS384': 'sha384', 'RS512': 'sha512'}
+
 pkcs_regex = (b'\x01\xff+\x00(?P<algorithm>' +
               b'|'.join(sorted(re.escape(asn1) for asn1 in ASN1_HASH.keys())) +
               b')(?P<hash>.+)')
 
 pkcs_signature = re.compile(pkcs_regex, re.DOTALL)
 
+class BadSignatureError(ValueError): pass
+
 class PublicKey(object):
     KTY = "RSA"
     
-    def __init__(self, n, e):
+    def __init__(self, n, e, alg='RS256'):
         self.e = e
         self.n = n
+        self.alg = alg
      
     @classmethod
     def from_jwk(cls, jwk):
@@ -49,14 +56,15 @@ class PublicKey(object):
         if jwk['kty'] != cls.KTY:
             raise ValueError("Not a {0} key".format(cls.KTY))
         n = b64_to_int(as_binary(jwk['n']))
-        e = b64_to_int(as_binary(jwk['e']))
-        return cls(n, e)
+        e = b64_to_int(as_binary(jwk['e']))        
+        return cls(n, e, jwk['alg'])
     
     def to_jwk(self):
         """Return a JSON Web Key for this key."""
         jwk = {'kty':self.KTY, 
                'n':as_text(int_to_b64(self.n)),
-               'e':as_text(int_to_b64(self.e))}
+               'e':as_text(int_to_b64(self.e)),
+               'alg':self.alg}
         return jwk
     
     def verify(self, message, signature):
@@ -68,15 +76,18 @@ class PublicKey(object):
         plain_signature_bytes = int_to_bytes(plain_signature)
         match = pkcs_signature.match(plain_signature_bytes)
         if match:
+            asn1_hash = ASN1_HASH[match.group(1)]
+            alg_hash = ALG_HASH[self.alg]
+            if asn1_hash != alg_hash:
+                raise BadSignatureError("Key and signature algorithm do not match", asn1_hash, alg_hash)
             hash = hashlib.new(ASN1_HASH[match.group(1)])
             hash.update(message)
             if hash.digest() == match.group(2):
                 return message            
-        raise ValueError("Bad signature")
+        raise BadSignatureError("Bad signature")
     
     def __repr__(self):
         return "{0}(n={1},e={2})".format(self.__class__.__name__, self.n, self.e)
-
 
 def decrypt(ciphertext, pub):
     """RSA decryption on integers"""
@@ -116,3 +127,29 @@ def as_binary(text):
 
 def as_text(data):
     return data.decode('latin1')
+
+def _verify_jwt(token, jwks):
+    message, _, signature = token.rpartition('.')
+    header, payload = message.split('.')
+    header = json.loads(urlsafe_b64decode(header))
+    kid = header['kid']
+    try:
+        jwk = next(key for key in jwks['keys'] if key['kid'] == kid)
+    except StopIteration:
+        raise BadSignatureError("Key not found in jwks")
+    pk = PublicKey.from_jwk(jwk)
+    verified = pk.verify(message, urlsafe_b64decode(signature))
+    return verified
+
+def verify_jwt(token, jwks):
+    """Verify the signature on a RSA-signed JSON web token.
+    Does not check any of the claims within the token.
+
+    token: Full signed token
+    jwks: JSON web key set (object with {"keys":[...]})
+
+    Return decoded payload.
+    """
+    verified = _verify_jws(token, jwks)
+    header, _, payload = verified.partition('.')
+    return json.loads(urlsafe_b64decode(payload))
--- tests.py
+++ tests.py
@@ -9,6 +9,8 @@ from nose.tools import eq_, raises
 keys = [            
         {'e': 'AQAB', 
          'kty': 'RSA', 
+         'alg': 'RS384',
+         # 'use': 'sig',    # TODO
          'n': 'tbg-f-iI33OW7Pll0eah8mmAz-5kQWntKRzP3Bd3dB93523t0ZQEhS17wR4TLOgKBGDGhncMvWUH53-pPWxkDanPXpQ53mK4McQfA6PE__XrgUI_DfpuK-46HJecJnyKcghrSUKkKAM9ZU46zVRsmr84t8IKBwRwzdqfOT3UJEbB3ktqw-1UNsz0ZmBAeZXnETbBGwSo3tTeHOVq0E6kYqmlaO0Eu1jfN8mxLhc1x7_9osjsbO0pkJTchdaVBl7MLpmYNfwlh3eAzir__avGXetJa9fpsP2KAG0_6OSlTh2MzwyuTRTqmU0rQQOHscqoM8VubFH5odRcca3lHFFb0Q'}
        ]
 
@@ -32,7 +34,8 @@ oUQDQgAE5tG+pwfXBazh6HY8/Hs/j4cW0gcw485nUkoR3cgxNyAbY9tXsMAJMEIr
 """
 
 def test_asn1lette():
-    pk = rsalette.PublicKey(*asn1lette.parse_pem(iter(pem.splitlines())))
+    # Do the ASN.1 keys contain an equivalent to alg?
+    pk = rsalette.PublicKey(*asn1lette.parse_pem(iter(pem.splitlines())), alg='RS384')
     eq_(pk.to_jwk(), keys[0])
 
 def test_ec_pem():
@@ -97,6 +100,7 @@ def sign_with_openssl(message, key_filename, digest='sha256'):
 
 def _test_signature(digest='sha256'):
     our_pubkey = rsalette.PublicKey.from_jwk(keys[0])
+    our_pubkey.alg = {'sha256':'RS256', 'sha384':'RS384', 'sha512':'RS512'}[digest]
     here = os.path.abspath(os.path.dirname(__file__))
     
     message = json.dumps(keys[0]).encode('utf-8')
